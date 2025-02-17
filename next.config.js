85b627081d6ba753a7a16fe73eb5cbef1d2062c1
c0rz <sinambelaalfredo@gmail.com>
2025-02-17 10:00:38 +0700

    deployment

--- next.config.js
+++ next.config.js
@@ -5,8 +5,8 @@ const nextConfig = {
     formats: ['image/avif', 'image/webp'],
     unoptimized: true // karena Anda menggunakan next export
   },
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/{repo-name}' : '', // Ganti {repo-name} dengan nama repository Anda
-  basePath: process.env.NODE_ENV === 'production' ? '/{repo-name}' : '', // Ganti {repo-name} dengan nama repository Anda
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/c0rz.github.io' : '', // Sesuaikan dengan nama repo Anda
+  basePath: process.env.NODE_ENV === 'production' ? '/c0rz.github.io' : '', // Sesuaikan dengan nama repo Anda
 }
 
 module.exports = nextConfig
