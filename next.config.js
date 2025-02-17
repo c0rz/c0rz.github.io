e87afa5da424026ccf9f12bc2e4b1c3d2ed81f69
c0rz <sinambelaalfredo@gmail.com>
2025-02-17 10:04:42 +0700

    Trigger rebuild

--- next.config.js
+++ next.config.js
@@ -5,8 +5,9 @@ const nextConfig = {
     formats: ['image/avif', 'image/webp'],
     unoptimized: true // karena Anda menggunakan next export
   },
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/c0rz.github.io' : '', // Sesuaikan dengan nama repo Anda
-  basePath: process.env.NODE_ENV === 'production' ? '/c0rz.github.io' : '', // Sesuaikan dengan nama repo Anda
+  // Karena ini adalah user/organization page, tidak perlu basePath dan assetPrefix
+  assetPrefix: process.env.NODE_ENV === 'production' ? '' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '' : ''
 }
 
 module.exports = nextConfig
