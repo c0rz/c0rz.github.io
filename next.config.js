206e895d220c303f667220321836d2443efa4c93
c0rz <sinambelaalfredo@gmail.com>
2025-02-17 10:07:49 +0700

    Update dependencies to fix security vulnerabilities

--- next.config.js
+++ next.config.js
@@ -1,13 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',  // Tambahkan ini untuk Next.js 13+
   images: {
     formats: ['image/avif', 'image/webp'],
     unoptimized: true // karena Anda menggunakan next export
   },
-  // Karena ini adalah user/organization page, tidak perlu basePath dan assetPrefix
-  assetPrefix: process.env.NODE_ENV === 'production' ? '' : '',
-  basePath: process.env.NODE_ENV === 'production' ? '' : ''
+  // Hapus assetPrefix dan basePath karena ini adalah user page
 }
 
 module.exports = nextConfig
